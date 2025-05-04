4d60e98b3cde1b752bd1c8fabc33e37f933c9127
KMRSKPHALGUN <phalgunkothamasu@gmail.com>
2025-05-04 21:50:36 +0530

    Spelling Mistake

--- controllers/Residential_rent.js
+++ controllers/Residential_rent.js
@@ -84,7 +84,7 @@ exports.residentialRent = async(req, res) => {
         res.status(201).json({ message: 'Property Posted Successfully' });
 
     }catch(error){
-        console.error('Multer error or route crash:', err);
+        console.error('Multer error or route crash:', error);
         res.status(500).json({ error: 'Something went wrong' });
     }
 }
